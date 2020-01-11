0654f81b3de22a6c7cd54fae623440e7f6fbad06
Yakser <iaksanov2044@mail.ru>
2020-01-11 18:16:41 +0400

    Set font Comic Sans MS 10; Change geometry to 250x230

--- main.py
+++ main.py
@@ -61,47 +61,47 @@ def skip():
 
 master = tkinter.Tk()
 master.title("Periodic table guessing game")
-master.geometry("220x175")
+master.geometry("250x230")
 master.resizable(0, 0)
 
 periodic = periodic_table.get_periodic_table()
 STREAK = 0
 rand_num = random.randint(1, 112)
 
-lbl_num = tkinter.Label(text=str(rand_num))
+lbl_num = tkinter.Label(text=str(rand_num), font=('Comic Sans MS', 10))
 lbl_num.grid(column=1, row=0)
 
-lbl_el = tkinter.Label(text='Element:')
+lbl_el = tkinter.Label(text='Element:', font=('Comic Sans MS', 10))
 lbl_el.grid(column=0, row=1)
 
 entry = tkinter.Entry(width=5)
 entry.grid(column=1, row=1, padx=5)
 
-btn_submit = tkinter.Button(text='Submit', command=check)
+btn_submit = tkinter.Button(text='Submit', command=check, font=('Comic Sans MS', 10))
 btn_submit.grid(column=2, row=1, padx=5)
 
-btn_gen = tkinter.Button(text='Generate new', command=new_num)
+btn_gen = tkinter.Button(text='Generate new', command=new_num, font=('Comic Sans MS', 10))
 btn_gen.grid(column=0, row=4, pady=5, padx=1)
 
-btn_ans = tkinter.Button(text='Answer', command=get_ans)
+btn_ans = tkinter.Button(text='Answer', command=get_ans, font=('Comic Sans MS', 10))
 btn_ans.grid(column=2, row=4)
 
-lbl_ans = tkinter.Label(text='')
+lbl_ans = tkinter.Label(text='', font=('Comic Sans MS', 10))
 lbl_ans.grid(column=2, row=0)
 
-lbl_status = tkinter.Label(text='')
+lbl_status = tkinter.Label(text='', font=('Comic Sans MS', 10))
 lbl_status.grid(column=1, row=2, columnspan=2, sticky='NW')
 
-lbl_hints = tkinter.Label(text='')
+lbl_hints = tkinter.Label(text='', font=('Comic Sans MS', 10))
 lbl_hints.grid(column=0, row=3, columnspan=3)
 
-lbl_attempts = tkinter.Label(text='Attempts:' + str(STREAK), fg='green')
+lbl_attempts = tkinter.Label(text='Attempts:' + str(STREAK), fg='green', font=('Comic Sans MS', 10))
 lbl_attempts.grid(column=1, row=4, padx=2)
 
-btn_skip = tkinter.Button(text='Skip', command=skip)
+btn_skip = tkinter.Button(text='Skip', command=skip, font=('Comic Sans MS', 10))
 btn_skip.grid(column=1, row=6)
 
-btn_quit = tkinter.Button(text='Exit', command=quit, width=10)
+btn_quit = tkinter.Button(text='Exit', command=quit , font=('Comic Sans MS', 10))
 btn_quit.grid(column=2, row=7)
 
 master.mainloop()
